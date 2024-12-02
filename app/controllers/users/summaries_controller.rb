e0718b753c3cbcce82f6b990b971edf64752bf5f
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-12-02 19:18:05 +0900

    セッションに保存して表示させるところまで

--- app/controllers/users/summaries_controller.rb
+++ app/controllers/users/summaries_controller.rb
@@ -1,9 +1,12 @@
 class Users::SummariesController < Users::ApplicationController
   def create
+    delete_session[:summary] # セッションを削除する
     response_log = gitlog.request
     response_calender = calendar.request
     commits = response_log + response_calender
-    render plain: openai.get_response_by(commits)
+    @summary = openai.get_response_by(commits)
+    session[:summary] = @summary　 # セッションに保存する
+    redirect_to users_root_path
   end
 
   private
